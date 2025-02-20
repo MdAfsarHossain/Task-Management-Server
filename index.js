0c326f12c83eefc0d486c09f87cdb42f9c961a26
MdAfsarHossain <afsar291722@gmail.com>
2025-02-20 22:49:38 +0600

    Cors options code added

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://task-management-6aed6.web.app",
+  ],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
