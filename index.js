d7971e37e2dd7fe7d09d15966248140df984a658
MdAfsarHossain <afsar291722@gmail.com>
2025-02-20 22:52:15 +0600

    MongoDB async function code added

--- index.js
+++ index.js
@@ -31,6 +31,19 @@ const client = new MongoClient(uri, {
   },
 });
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("Welcome To The Task Management System Sercer site..");
 });
